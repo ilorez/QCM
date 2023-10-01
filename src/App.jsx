818754c2c8a8cfd009bd8e5e1a434e193a61ae3b
Z-Najdaoui <zobirnajdaoui@gmail.com>
2023-10-02 00:39:18 +0100

    remove not using link import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import QCM from './pages/mainQcm'
 
